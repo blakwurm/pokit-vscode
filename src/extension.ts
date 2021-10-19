ee116f64a74e7816938f45d3e59099f1ace0b729
Jordan LaPrise <officialtoxicdub@gmail.com>
2021-10-18 22:01:04 -0800

    pass vscode state to scene editor

--- src/extension.ts
+++ src/extension.ts
@@ -3,11 +3,29 @@
 import * as fs from 'fs';
 import * as vscode from 'vscode';
 import * as path from 'path';
-import * as express from 'express';
+import { CartManifest, EntityStub, SceneStub } from './pokit.types';
 
 let editor: vscode.WebviewPanel;
 let ctx: vscode.ExtensionContext;
 let disposed = true;
+let watching = false;
+
+enum ToolType {
+	BRUSH,
+	SELECT,
+	PAN
+}
+
+export interface AppData {
+	handling?: boolean
+	manifest: CartManifest
+	entities: Record<string, EntityStub>
+	scenes: Record<string, SceneStub>
+	currentScene: string
+	currentBrush: string
+	currentTool: ToolType
+	inspecting: [string, string, number]
+}
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -19,6 +37,75 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('pokit-vscode.editor', startEditor);
 
 	context.subscriptions.push(disposable);
+}
+
+function makeFileSystemWatchers(workspace: string, entities: string, scenes: string) {
+	let cartWatcher = vscode.workspace.createFileSystemWatcher(
+		new vscode.RelativePattern(workspace, "cart.json")
+	);
+	let entityWatcher = vscode.workspace.createFileSystemWatcher(
+		new vscode.RelativePattern(entities, "*.json")
+	);
+	let sceneWatcher = vscode.workspace.createFileSystemWatcher(
+		new vscode.RelativePattern(scenes, "*.json")
+	);
+	cartWatcher.onDidChange(onCartChange);
+	entityWatcher.onDidCreate(onEntityChange);
+	entityWatcher.onDidChange(onEntityChange);
+	entityWatcher.onDidDelete(onEntityDelete);
+	sceneWatcher.onDidCreate(onSceneChange);
+	sceneWatcher.onDidChange(onSceneChange);
+	sceneWatcher.onDidDelete(onSceneDelete);
+	watching = true;
+}
+
+function sendEvent(evt: string, content: vscode.Uri, skipData?: boolean, defaults?: any) {
+	defaults = defaults || {};
+	if(disposed) return;
+	let split = content.toString().split('/');
+	let fileSplit = split[split.length-1].split('.');
+	let name = fileSplit[fileSplit.length-2];
+	if(skipData){
+		editor.webview.postMessage({evt,name});
+		return;
+	}
+	vscode.workspace.fs.readFile(content).then((c)=>{
+		let data = JSON.parse(c.toString());
+		data = Object.assign(defaults, data);
+		editor.webview.postMessage({
+			evt,
+			name,
+			data
+		})
+	});
+}
+
+function onCartChange(f: vscode.Uri) {
+	sendEvent("cart_change", f);
+}
+
+function onEntityChange(f: vscode.Uri) {
+	sendEvent('entity_change', f, false, {
+		inherits: [],
+		components: {},
+		timestamp: Date.now()
+	});
+}
+
+function onEntityDelete(f: vscode.Uri) {
+	sendEvent("entity_delete", f, true);
+}
+
+function onSceneChange(f: vscode.Uri) {
+	sendEvent("scene_change", f, false, {
+		systems: [],
+		entities: {},
+		timestamp: Date.now()
+	});
+}
+
+function onSceneDelete(f: vscode.Uri) {
+	sendEvent("scene_delete", f, true);
 }
 
 function startEditor() {
@@ -48,21 +135,32 @@ function startEditor() {
 			}
 		)
 		editor.onDidDispose(()=>disposed=true);
-		createEditor();
+		createEditor(workspace, entityFolder, sceneFolder);
 		disposed = false;
 	} else {
 		editor.reveal();
 	}
 }
 
-function createEditor() {
+async function createEditor(workspace: string, entityFolder: string, sceneFolder: string) {
 	let j = path.join;
 	let tools = j(ctx.extensionPath, 'pokit-web-editor', 'public');
 	let build = j(tools, 'build');
 	let js = j(build, 'bundle.js');
 	let buildcss = prepareCss(build, 'bundle.css');
 	let globalcss = prepareCss(tools, 'global.css');
-	editor.webview.html = getPageSrc(js,globalcss,buildcss);
+	editor.webview.onDidReceiveMessage(onStateUpdate);
+	let state = await initEditorState(workspace);
+	editor.webview.html = getPageSrc(js,globalcss,buildcss,state);
+	if(!watching) makeFileSystemWatchers(workspace, entityFolder, sceneFolder);
+}
+
+function sleep(t: number) {
+	return new Promise((resolve)=>setTimeout(resolve, t));
+}
+
+function onStateUpdate(a: AppData) {
+	if(!a.handling)console.log(a);
 }
 
 function prepareCss(root: string, file: string) {
@@ -76,7 +174,7 @@ function prepareCss(root: string, file: string) {
 	});
 }
 
-function getPageSrc(js: string, globalcss: string, buildcss: string) {
+function getPageSrc(js: string, globalcss: string, buildcss: string, state: string) {
 	let uri = vscode.Uri.file
 	let jsUri = editor.webview.asWebviewUri(uri(js)).toString();
 
@@ -86,6 +184,7 @@ function getPageSrc(js: string, globalcss: string, buildcss: string) {
 		if (!window.process) {
 			window.process = {env: {NODE_ENV:"production"}}
 		}
+		window.__pokit_state=JSON.parse(\`${state}\`);
 	</script>
 <head>
 	<meta charset='utf-8'>
@@ -103,6 +202,65 @@ function getPageSrc(js: string, globalcss: string, buildcss: string) {
 </html>
 
 `;
+}
+
+async function initEditorState(ws: string) {
+	let uri = vscode.Uri.file;
+	let entityFolder = uri(path.join(ws, 'entities'));
+	let sceneFolder = uri(path.join(ws, 'scenes'));
+	let manifestPath = uri(path.join(ws, 'cart.json'));
+	let spritesPath = uri(path.join(ws, 'sprites.png'));
+	let fs = vscode.workspace.fs;
+	let manifestBytes = await fs.readFile(manifestPath);
+	let spritesBytes = await fs.readFile(spritesPath);
+	let manifest = JSON.parse(manifestBytes.toString());
+	let entityFiles = await fs.readDirectory(entityFolder);
+	let sceneFiles = await fs.readDirectory(sceneFolder);
+	let entities: Record<string,EntityStub> = {};
+	let scenes: Record<string,SceneStub> = {};
+	for(let [file, type] of entityFiles) {
+		let split = file.split('.');
+		if(type === 1 && split[split.length-1] === 'json') {
+			let name = split.slice(0, -1).join('.');
+			let fileUri = uri(path.join(ws, 'entities', file));
+			let eString = (await fs.readFile(fileUri)).toString();
+			let e = JSON.parse(eString);
+			e = Object.assign({
+				inherits: [],
+				components: {},
+				timestamp: Date.now()
+			},e)
+			entities[name] = e;
+		}
+	}
+	for(let [file, type] of sceneFiles) {
+		let split = file.split('.');
+		if(type === 1 && split[split.length-1] === 'json') {
+			let name = split.slice(0, -1).join('.');
+			let fileUri = uri(path.join(ws, 'scenes', file));
+			let sString = (await fs.readFile(fileUri)).toString();
+			let s = JSON.parse(sString);
+			s = Object.assign({
+				systems: [],
+				entities: {},
+				timestamp: Date.now()
+			},s)
+			scenes[name] = s;
+		}
+	}
+	return JSON.stringify({
+		state: {
+			manifest,
+			entities,
+			scenes,
+			currentScene: manifest.defaultScene,
+			currentBrush: [...Object.keys(entities)][0],
+			currentTool: ToolType.SELECT,
+			inspecting: ["",0,0],
+
+		},
+		spritemap: Buffer.from(spritesBytes).toString('base64')
+	})
 }
 
 // this method is called when your extension is deactivated
---
+++ src/pokit.types.d.ts
@@ -0,0 +1,40 @@
+export interface CartManifest {
+    author: string;
+    name: string;
+    defaultScene: string;
+    modules: string[];
+    scripts: string[];
+    entities: { [name:string]: EntityStub };
+    scenes: { [name:string]: SceneStub };
+    sceneShards: string[];
+    entityShards: string[];
+  }
+  
+  export interface SceneStub {
+    systems?: string[];
+    entities: { [stub:string]: Identity[] };
+    timestamp?: number;
+  }
+  
+  export interface EntityStub {
+    inherits: string[];
+    components: any;
+    timestamp?: number;
+  }
+
+  export interface Identity {
+    id?: string;
+    parent?: string;
+    persistent?: boolean;
+    bounds?: Vector;
+    position?: Vector;
+    z?: number;
+    depth?: number;
+    scale?: Vector;
+    rotation?: number;
+  }
+
+  export interface Vector {
+      x: number
+      y: number
+  }
