7094291ed4f4b520ee8c309a52cdcd7725100e65
Jordan LaPrise <officialtoxicdub@gmail.com>
2021-10-19 19:20:17 -0800

    remove fs dependency and update cart dynamically

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as fs from 'fs';
 import * as vscode from 'vscode';
-import * as path from 'path';
+import {join as j} from 'path';
 import { CartManifest, EntityStub, SceneStub } from './pokit.types';
 
 let editor: vscode.WebviewPanel;
@@ -74,10 +73,14 @@ function getName(uri: vscode.Uri) {
 	return fileSplit[fileSplit.length-2];
 }
 
-async function loadUri(uri: vscode.Uri, defaults?: any) {
-	defaults = defaults || {};
+async function loadUri(uri: vscode.Uri) {
 	let c = await vscode.workspace.fs.readFile(uri)
-	let str = c.toString();
+	return c.toString();
+}
+
+async function loadJsonUri(uri: vscode.Uri, defaults?: any) {
+	defaults = defaults || {};
+	let str = await loadUri(uri);
 	try {
 		let data = JSON.parse(str);
 		return Object.assign(defaults, data);
@@ -87,7 +90,7 @@ async function loadUri(uri: vscode.Uri, defaults?: any) {
 }
 
 async function onCartChange(f: vscode.Uri) {
-	let data = await loadUri(f);
+	let data = await loadJsonUri(f);
 	if(!data)return;
 	let name = getName(f);
 	sendEvent("cart_change", name, data);
@@ -95,7 +98,7 @@ async function onCartChange(f: vscode.Uri) {
 
 async function onEntityChange(f: vscode.Uri) {
 	let name = getName(f);
-	let data = await loadUri(f, {
+	let data = await loadJsonUri(f, {
 		inherits: [],
 		components: {},
 		timestamp: Date.now()
@@ -113,7 +116,7 @@ async function onEntityDelete(f: vscode.Uri) {
 
 async function onSceneChange(f: vscode.Uri) {
 	let name = getName(f);
-	let data = await loadUri(f, {
+	let data = await loadJsonUri(f, {
 		systems: [],
 		entities: {},
 		timestamp: Date.now()
@@ -129,7 +132,7 @@ async function onSceneDelete(f: vscode.Uri) {
 	sendEvent("scene_delete", name);
 }
 
-function startEditor() {
+async function startEditor() {
 	let folders = vscode.workspace.workspaceFolders;
 	if(!folders) {
 		vscode.window.showErrorMessage("Must have active workspace to edit cart")
@@ -137,10 +140,10 @@ function startEditor() {
 	}
 
 	workspace = folders[0].uri.fsPath;
-	const entityFolder = path.join(workspace, 'entities');
-	const sceneFolder = path.join(workspace, 'scenes')
+	const entityFolder = j(workspace, 'entities');
+	const sceneFolder = j(workspace, 'scenes')
 
-	if(!fs.existsSync(path.join(workspace, 'cart.json'))) {
+	if(!await exists(vscode.Uri.file(j(workspace, 'cart.json')))) {
 		vscode.window.showErrorMessage("Workspace does not have valid cart.json")
 		return;
 	}
@@ -164,12 +167,11 @@ function startEditor() {
 }
 
 async function createEditor(workspace: string, entityFolder: string, sceneFolder: string) {
-	let j = path.join;
 	let tools = j(ctx.extensionPath, 'pokit-web-editor', 'public');
 	let build = j(tools, 'build');
 	let js = j(build, 'bundle.js');
-	let buildcss = prepareCss(build, 'bundle.css');
-	let globalcss = prepareCss(tools, 'global.css');
+	let buildcss = await prepareCss(build, 'bundle.css');
+	let globalcss = await prepareCss(tools, 'global.css');
 	editor.webview.onDidReceiveMessage(onStateUpdate);
 	let state = await initEditorState(workspace);
 	editor.webview.html = getPageSrc(js,globalcss,buildcss,state);
@@ -181,8 +183,8 @@ async function onStateUpdate(msg: AppData) {
 	delete msg.entities['__DEFAULT_PARENT__']
 	let {del:edel, change:echange} = getChanges(entities, msg.entities);
 	let {del:sdel, change:schange} = getChanges(scenes, msg.scenes);
-	let entityPath = path.join(workspace, 'entities');
-	let scenePath = path.join(workspace, 'scenes');
+	let entityPath = j(workspace, 'entities');
+	let scenePath = j(workspace, 'scenes');
 	for(let [k,v] of Object.entries(echange)) {
 		await updateFile(entityPath, k, v);
 		entities[k] = v;
@@ -190,6 +192,23 @@ async function onStateUpdate(msg: AppData) {
 	for(let [k,v] of Object.entries(schange)) {
 		await updateFile(scenePath, k, v);
 		scenes[k] = v;
+	}
+	await updateCart();
+}
+
+async function updateCart() {
+	let cartPath = j(workspace, 'cart.json');
+	let uri = vscode.Uri.file(cartPath);
+	let file = await vscode.workspace.openTextDocument(uri);
+	let obj: CartManifest = JSON.parse(file.getText());
+	let cartEntities = obj.entityShards;
+	let cartScenes = obj.sceneShards;
+	let memEntities = [...Object.keys(entities)].map(e=>e+".json");
+	let memScenes = [...Object.keys(scenes)].map(s=>s+".json");
+	if(diff(cartEntities.sort(), memEntities.sort()) || diff(cartScenes.sort(), memScenes.sort())) {
+		obj.entityShards = memEntities;
+		obj.sceneShards = memScenes;
+		updateFile(workspace, "cart", obj);
 	}
 }
 
@@ -204,7 +223,7 @@ async function exists(uri: vscode.Uri) {
 }
 
 async function updateFile(root: string, name: string, content: any) {
-	let uri = vscode.Uri.file(path.join(root,name+'.json'));
+	let uri = vscode.Uri.file(j(root,name+'.json'));
 	let txt = JSON.stringify(content, null, 2);
 	if(!await exists(uri)) {
 		await vscode.workspace.fs.writeFile(uri, new Uint8Array());
@@ -220,11 +239,11 @@ function deleteFile(path: string, name: string) {
 
 }
 
-function prepareCss(root: string, file: string) {
-	let p = path.join(root, file);
-	let txt = fs.readFileSync(p, 'utf-8');
+async function prepareCss(root: string, file: string) {
+	let p = j(root, file);
+	let txt = await loadUri(vscode.Uri.file(p));
 	return txt.replace(/(?:url\('(?<url>.*(?='))'\))/, (t,url)=>{
-		let n = path.join(root,url);
+		let n = j(root,url);
 		let uri = vscode.Uri.file(n);
 		let webUri = editor.webview.asWebviewUri(uri).toString();
 		return `url('${webUri}')`
@@ -263,10 +282,10 @@ function getPageSrc(js: string, globalcss: string, buildcss: string, state: stri
 
 async function initEditorState(ws: string) {
 	let uri = vscode.Uri.file;
-	let entityFolder = uri(path.join(ws, 'entities'));
-	let sceneFolder = uri(path.join(ws, 'scenes'));
-	let manifestPath = uri(path.join(ws, 'cart.json'));
-	let spritesPath = uri(path.join(ws, 'sprites.png'));
+	let entityFolder = uri(j(ws, 'entities'));
+	let sceneFolder = uri(j(ws, 'scenes'));
+	let manifestPath = uri(j(ws, 'cart.json'));
+	let spritesPath = uri(j(ws, 'sprites.png'));
 	let fs = vscode.workspace.fs;
 	let manifestBytes = await fs.readFile(manifestPath);
 	let spritesBytes = await fs.readFile(spritesPath);
@@ -279,7 +298,7 @@ async function initEditorState(ws: string) {
 		let split = file.split('.');
 		if(type === 1 && split[split.length-1] === 'json') {
 			let name = split.slice(0, -1).join('.');
-			let fileUri = uri(path.join(ws, 'entities', file));
+			let fileUri = uri(j(ws, 'entities', file));
 			let eString = (await fs.readFile(fileUri)).toString();
 			let e = JSON.parse(eString);
 			e = Object.assign({
@@ -294,7 +313,7 @@ async function initEditorState(ws: string) {
 		let split = file.split('.');
 		if(type === 1 && split[split.length-1] === 'json') {
 			let name = split.slice(0, -1).join('.');
-			let fileUri = uri(path.join(ws, 'scenes', file));
+			let fileUri = uri(j(ws, 'scenes', file));
 			let sString = (await fs.readFile(fileUri)).toString();
 			let s = JSON.parse(sString);
 			s = Object.assign({
@@ -305,6 +324,7 @@ async function initEditorState(ws: string) {
 			scenes[name] = s;
 		}
 	}
+	await updateCart();
 	return JSON.stringify({
 		state: {
 			manifest,
@@ -319,7 +339,7 @@ async function initEditorState(ws: string) {
 		spritemap: Buffer.from(spritesBytes).toString('base64')
 	})
 }
-let log = false;
+let logDiff = false;
 function getChanges(original: Record<string,any>, modified: Record<string,any>) {
 	let keys = new Set([...Object.keys(original),...Object.keys(modified)]);
 	let del = new Set<string>();
@@ -333,24 +353,24 @@ function getChanges(original: Record<string,any>, modified: Record<string,any>) 
 
 function diff(original: any, modified: any) {
 	if(Array.isArray(original)) {
-		if(original.length !== modified.length) {if(log)console.log('Array length mismatch'); return true; }
+		if(original.length !== modified.length) {if(logDiff)console.log('Array length mismatch'); return true; }
 		for(let i in original) {
-			if(typeof original[i] !== typeof modified[i]) {if(log)console.log('types:', typeof original[i], 'and',typeof modified[i], 'dont match');return true};
+			if(typeof original[i] !== typeof modified[i]) {if(logDiff)console.log('types:', typeof original[i], 'and',typeof modified[i], 'dont match');return true};
 			if(typeof original[i] === 'object') {
 				if(diff(original[i], modified[i])) return true;
 			}
-			else if(original[i] !== modified[i]) {if(log)console.log('values:',original[i],'and',modified[i],'dont match');return true};
+			else if(original[i] !== modified[i]) {if(logDiff)console.log('values:',original[i],'and',modified[i],'dont match');return true};
 		}
 		return false;
 	}
 	let keys = new Set([...Object.keys(original),...Object.keys(modified)]);
 	for(let k of keys) {
-		if(!(k in original) || !(k in modified)){ if(log)console.log(k,'not present in both objects'); return true; }
-		if(typeof original[k] !== typeof modified[k]){ if(log)console.log(k, 'types:', typeof original[k], 'and', typeof modified[k], 'do not match'); return true; }
+		if(!(k in original) || !(k in modified)){ if(logDiff)console.log(k,'not present in both objects'); return true; }
+		if(typeof original[k] !== typeof modified[k]){ if(logDiff)console.log(k, 'types:', typeof original[k], 'and', typeof modified[k], 'do not match'); return true; }
 		if(typeof original[k] === 'object') {
 			if(diff(original[k], modified[k])){ return true; }
 		}
-		else if(original[k]!==modified[k]){ if(log)console.log(k, 'values:', original[k], 'and', modified[k], 'do not match'); return true; }
+		else if(original[k]!==modified[k]){ if(logDiff)console.log(k, 'values:', original[k], 'and', modified[k], 'do not match'); return true; }
 	}
 	return false;
 }
